b2bc632d6af72d468975639923a4a65b68740a76
liuya <1522962872@qq.com>
2020-08-24 23:20:03 +0800

    a

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '../dist'),
-    publicPath: '/', // 根路径
+    publicPath: './', // 根路径
   },
   module: {
     rules: [
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 // import _ from 'lodash'
-// var element = document.createElement('div')
-// element.innerHTML = _.join(['dell', 'lee'], '_')
-// document.body.appendChild(element)
-console.log(process.env.NODE_ENV)
+// async function getComponent() {
+//   // 异步加载lodash.js-----可以实现懒加载
+//   const { default: _ } = await import(/* webpackChunkName:"lodash" */ 'lodash')
+//   console.log(_, 'element')
+//   var element = document.createElement('div')
+//   element.innerHTML = _.join(['dell', 'lee'], '_')
+//   return element
+// }
+// document.addEventListener('click', () => {
+//   getComponent().then((element) => {
+//     document.body.appendChild(element)
+//   })
+// })
 
-function getComponent() {
-  // 异步加载lodash.js-----可以实现懒加载
-  return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default: _ }) => {
-    var element = document.createElement('div')
-    element.innerHTML = _.join(['dell', 'lee'], '_')
-    return element
-  })
-}
 document.addEventListener('click', () => {
-  getComponent().then((element) => {
-    document.body.appendChild(element)
+  import('./click').then(({default: func}) => {
+    func()
   })
 })
 
